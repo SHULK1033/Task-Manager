8a2166cf5e59c26806454cd332b429c5fcd0b6df
SHULK1033 <andres10333@hotmail.com>
2020-05-10 22:51:49 -0500

    creando aplicacion para la gestion de stock en react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [userName, setUserName] = useState('andres');
+  const [taskItems, setTaskItems] = useState([
+    { name: 'Task One', done: false },
+    { name: 'Task Two', done: false },
+    { name: 'Task Three', done: true },
+    { name: 'Task Four', done: false },
+  ]);
+
+  const taskTableRows = () => {
+    return taskItems.map(task => (
+      <tr key={task.name}>
+        <td>{task.name}</td>
+      </tr>
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskTableRows()}
+        </tbody>
+      </table>
     </div>
   );
 }
